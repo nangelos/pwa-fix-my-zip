2b0536c14437655018d3aaeb42700838458b4e89
nangelos <nicksangelos@gmail.com>
2018-04-26 16:10:20 -0500

    fixing to backside camera

--- src/components/camera.js
+++ src/components/camera.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Image = () => {
   return (
     <div>
-      <input id="image-input" accept="image" type="file" capture="user" />
+      <input
+        id="image-input"
+        accept="image"
+        type="file"
+        capture="environment"
+      />
       <script>
         {/* {const fileInput = document.getElementById('image-input');
   fileInput.addEventListener('change', (e) => doSomethingWithFiles(e.target.files));
