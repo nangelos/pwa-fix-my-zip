8c751f89d5d982f1521f1a3b3f94d111ef128d4a
nangelos <nicksangelos@gmail.com>
2018-04-29 22:18:38 -0500

    rewording response

--- src/components/issue-response.js
+++ src/components/issue-response.js
@@ -10,6 +10,7 @@ const IssueResponse = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Thank you for helping us fix your neghborhood!</h2>
       </div>
+      <p className="App-intro">Let us know about any other issues.</p>
       <Navbar />
     </div>
   );
