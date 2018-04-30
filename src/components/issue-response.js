8ef4432e0bedc5e30b94ff71b5b0cb5b341a6d2a
nangelos <nicksangelos@gmail.com>
2018-04-29 22:14:22 -0500

    better response page

--- src/components/issue-response.js
+++ src/components/issue-response.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Navbar from '../components/navbar';
+import logo from '../styles/logo.png';
+import '../styles/App.css';
 
 const IssueResponse = () => {
   return (
-    <div>
-      <h2>Thank you helping us fix your neighborhood!</h2>
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Thank you for helping us fix your neghborhood!</h2>
+      </div>
+      <Navbar />
     </div>
   );
 };
